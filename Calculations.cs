295d498018adaa63b5fe3fad45f76e342b19a3d2
wilpet4 <william.petrik@gmail.com>
2022-02-22 15:31:04 +0100

    TEST

---
+++ Calculations.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherAppUI.Method_Classes;
+using WeatherDataLib;
+
+namespace WeatherAppUI
+{
+    public class Calculations
+    {
+        public double[] AvgPerQuarter(List<WeatherData> data)
+        {
+            DateTime time = data[0].Date;
+            double avgTemp = 0;
+            int points = 0;
+            int count = 0;
+            double[] tempsPerQuarter = new double[144]; //96 är pga att det alltid bara finns 96 kvartar på ett dygn, kommer aldrig att ändras!
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                if (time.AddMinutes(10) > data[i].Date)
+                {
+                    avgTemp += data[i].Temperature;
+                    points++;
+                }
+                if (time.AddMinutes(10) <= data[i].Date)
+                {
+                    tempsPerQuarter[count] = Math.Round(avgTemp / points, 1); // Lägger in medeltemperatur i listan.
+                    count++;
+                    points = 0; // Nollställer dessa inför nästa varv i loopen.
+                    avgTemp = 0;
+                    time = data[i].Date;
+                }
+            }
+
+            return tempsPerQuarter;
+        }
+        public float GetTempDiffPerDay(List<WeatherData> chart)
+        {
+            float max = 0;
+            float min = 100;
+            foreach (var c in chart)
+            {
+                if (c != null)
+                {
+                    if (c.Temperature > max)
+                        max = c.Temperature;
+                    if (c.Temperature < min)
+                        min = c.Temperature;
+                }
+            }
+            return max - min;
+
+        }
+        public float GetTempDiffPerDay(List<WeatherData> chart, List<WeatherData> chart2)
+        {
+            float max = 0;
+            float min = 100;
+            foreach (var c in chart)
+            {
+                if (c != null)
+                {
+                    if (c.Temperature > max)
+                    { max = c.Temperature; }
+
+                    if (c.Temperature < min)
+                        min = c.Temperature;
+                }
+            }
+            foreach (var c in chart2)
+            {
+                if (c != null)
+                {
+                    if (c.Temperature > max)
+                    { max = c.Temperature; }
+
+                    if (c.Temperature < min)
+                        min = c.Temperature;
+                }
+            }
+            return max - min;
+
+        }
+        public int DoorOpen()
+        {
+            //Dryness_LBox.Items.Clear();
+            if (ChartFunctions.outsideData.Count == 0 || ChartFunctions.insideData.Count == 0) return 0;
+
+            double[] outsideTemps = AvgPerQuarter(ChartFunctions.outsideData); // Lägger in genomsnittlig temperatur per kvart ute
+            double[] insideTemps = AvgPerQuarter(ChartFunctions.insideData); // Lägger in genomsnittlig temperatur per kvart inne
+            int DoorEstimate = 0;
+            int j = 0;
+
+            for (int i = 0; i < insideTemps.Length - 1; i++)
+            {
+
+                if (outsideTemps[i] < outsideTemps[i + 1] && insideTemps[i] > insideTemps[i + 1] * 1.005) //TODO: Statisk kalkyl, göra den dynamisk?
+                    DoorEstimate += 20;
+                j++;
+            }
+
+            return DoorEstimate;
+
+        }
+    }
+}
---
+++ Method Classes/DataFileRead.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherAppUI;
+
+namespace WeatherDataLib
+{
+    public static class DataFileRead
+    {
+        static StreamWriter sw = new StreamWriter("fel.txt");
+
+
+        public static void WriteToDatabase()
+        {
+            
+            var listToRead = new List<WeatherData>();
+            string path = @"TempFuktData.csv";
+            bool title = true;
+            string[] weatherFile = File.ReadAllLines(path);
+            var weatherData = new WeatherData();
+            var data = new Dictionary<Tuple<DateTime, string>, WeatherData>();
+            foreach (string line in weatherFile)
+            {
+                string[] columns = line.Split(',');
+                if (title == true)
+                {
+                    title = false;
+                    continue;
+                }
+
+                weatherData = (new WeatherData
+                {
+
+                    Date = DateTime.Parse(columns[0].ToString()),
+                    Temperature = CheckTemps(columns[2]),
+                    MoistLevel = int.Parse(columns[3]),
+                    Placement = columns[1]
+
+                });
+
+                listToRead.Add(weatherData);
+                
+                
+
+            }
+
+
+            RemoveDups(listToRead, data);
+           
+            using (var c = new WeatherContext())
+            {
+                c.WeatherDatas.AddRange(data.Values);
+                c.SaveChanges();
+            }
+
+        }
+
+        public static void RemoveDups(List<WeatherData> listToRead, Dictionary<Tuple<DateTime, string>, WeatherData> data)
+        {
+            foreach(var item in listToRead)
+            {
+                try
+                {
+                    data.Add(Tuple.Create(item.Date, item.Placement), item);
+                }
+                catch { }
+            }
+
+        }
+
+        static float CheckTemps(string toCheck)
+        {
+            if (float.TryParse(toCheck, NumberStyles.Float, CultureInfo.InvariantCulture, out float output))
+                return output;
+            else
+                        if (float.TryParse(toCheck.Substring(1), NumberStyles.Float, CultureInfo.InvariantCulture, out float output2))
+                return output2 * -1;
+            return -1000;
+        }
+
+    }
+}
+
